1cb5f7de469b0cf04668e4df43e11f7fefb9ae90
Andy Mockli <andymockli@gmail.com>
2015-10-12 13:14:54 -0600

    Added a specific exemption for nil-loops to instead treat them as an empty loop.

--- lib/sablon/operations.rb
+++ lib/sablon/operations.rb
@@ -10,6 +10,7 @@ module Sablon
     class Loop < Struct.new(:list_expr, :iterator_name, :block)
       def evaluate(context)
         value = list_expr.evaluate(context)
+        value = [] if value.nil?
         value = value.to_ary if value.respond_to?(:to_ary)
         raise ContextError, "The expression #{list_expr.inspect} should evaluate to an enumerable but was: #{value.inspect}" unless value.is_a?(Enumerable)
 
